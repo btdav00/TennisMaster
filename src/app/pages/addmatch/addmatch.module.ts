e2a1e291a945e1fed4e5b3180ac737873043df32
davide <davidebatti.26@gmail.com>
2022-08-19 19:36:06 +0200

    addmatch modified

--- src/app/pages/addmatch/addmatch.module.ts
+++ src/app/pages/addmatch/addmatch.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 import { IonicModule } from '@ionic/angular';
 
@@ -9,12 +9,13 @@ import { AddmatchPageRoutingModule } from './addmatch-routing.module';
 import { AddmatchPage } from './addmatch.page';
 
 @NgModule({
-  imports: [
-    CommonModule,
-    FormsModule,
-    IonicModule,
-    AddmatchPageRoutingModule
-  ],
+    imports: [
+        CommonModule,
+        FormsModule,
+        IonicModule,
+        AddmatchPageRoutingModule,
+        ReactiveFormsModule
+    ],
   declarations: [AddmatchPage]
 })
 export class AddmatchPageModule {}
